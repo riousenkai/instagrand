fdbacfd78233a610083924c500c2f42140c80aec
riousenkai <johnelijah.fajardo@gmail.com>
2021-11-17 19:51:40 -0600

    started on profile page

--- react-app/src/components/UserProfile/UserProfile.js
+++ react-app/src/components/UserProfile/UserProfile.js
@@ -2,12 +2,16 @@ import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router";
 import { findPosts } from "../../store/post";
+import { findFollows } from "../../store/follow";
 import "./UserProfile.css";
 
 const UserProfile = () => {
+  const main = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
   const { userId } = useParams();
-  const [user, setUser] = useState()
+  const [user, setUser] = useState();
+  const posts = useSelector(state => state.post)
+  const follows = useSelector(state => state.follow)
 
   useEffect(() => {
     if (!userId) {
@@ -21,16 +25,29 @@ const UserProfile = () => {
   }, [userId]);
 
   useEffect(() => {
+    dispatch(findFollows(+userId));
     dispatch(findPosts(+userId));
   }, [userId]);
 
   return (
     <div className="prof-main">
       <div className="prof-top">
-          <img className="prof-img" src={user?.image_url} />
+        <img className="prof-img" src={user?.image_url} />
+        <div className="prof-top-right">
           <div className="prof-top-top">
-
+            <div className="prof-name">{user?.username}</div>
+            {user?.id === main?.id ? (
+              <>
+                <button className="prof-edit">Edit Profile</button>
+              </>
+            ) : null}
           </div>
+          <div className="prof-count">
+              <div className="prof-posts"><span className="p-ct-bold">{posts[+userId]?.posts.length}</span> {posts[+userId]?.posts.length !== 1 ? "posts" : "post"}</div>
+              <div className="prof-posts"><span className="p-ct-bold">{follows[+userId]?.followers.length}</span> {follows[+userId]?.followers.length !== 1 ? "followers" : "follower"}</div>
+              <div className="prof-posts"><span className="p-ct-bold">{follows[+userId]?.following.length}</span> following</div>
+          </div>
+        </div>
       </div>
     </div>
   );
