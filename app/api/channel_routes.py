0647dfd55b52fe581e5fdff6f87bc0d0b24115b2
riousenkai <johnelijah.fajardo@gmail.com>
2021-11-23 17:38:29 -0600

    finished channel

--- app/api/channel_routes.py
+++ app/api/channel_routes.py
@@ -1,14 +1,14 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
-from app.models import Channel, User
+from app.models import DM_Channel, User
 from sqlalchemy import or_
 
 channel_routes = Blueprint('dm_channels', __name__)
 
-@channel_routes.rotue('/')
+@channel_routes.route('/')
 @login_required
 def get_channels():
 
-    channels = Channel.query.filter_by(user1_id = current_user.id or_ user2_id = current_user.id)
+    channels = DM_Channel.query.filter(or_(DM_Channel.user1_id==current_user.id, DM_Channel.user2_id==current_user.id)).all()
 
-    
+    return {'ok': [channel.to_dict() for channel in channels]}
