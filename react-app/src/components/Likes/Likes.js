5b3f965c14660eedf0ca908ffb6475363d142b99
riousenkai <johnelijah.fajardo@gmail.com>
2021-11-23 11:24:52 -0600

    fixing likes modal

--- react-app/src/components/Likes/Likes.js
+++ react-app/src/components/Likes/Likes.js
@@ -1,13 +1,24 @@
+import { useEffect } from "react";
+import { useHistory } from "react-router";
+import { useSelector, useDispatch } from "react-redux";
 import { close } from "./LikeIcons";
 import { useModal } from "../../context/UseModal";
 import "./Likes.css";
+import { findFollows } from "../../store/follow";
 
 const Likes = ({ users }) => {
+  const history = useHistory();
+  const dispatch = useDispatch();
   const { setLikes } = useModal();
+  const main = useSelector((state) => state.session.user);
+  const following = useSelector((state) => state.follow[main?.id]?.following);
+
+  useEffect(() => {
+    dispatch(findFollows(main?.id));
+  }, [main]);
 
   return (
     <div className="likes-main">
-      {console.log(users)}
       <div className="likes-top">
         <div className="likes-header">Likes</div>
         <div className="likes-close" onClick={() => setLikes(0)}>
@@ -15,15 +26,28 @@ const Likes = ({ users }) => {
         </div>
       </div>
       <div className="likes-bot">
-        {users && users.map((user) => {
-          <div className="likes-card-c">
-            <img className="likes-img" src={user.image_url} />
-            <div className="likes-details">
-              <div className="likes-username">{user.username}</div>
-              <div className="likes-name">{user.name}</div>
+        {console.log(users)}
+        {users.length > 0 &&
+          users.map((user) => (
+            <div className="likes-card">
+              <img
+                className="likes-img"
+                src={user.image_url}
+                onClick={() => history.push(`/users/${user.id}`)}
+              />
+              <div className="likes-details">
+                <div className="likes-username">{user.username}</div>
+                <div className="likes-name">{user.name}</div>
+              </div>
+              {user.id !== main.id ? (
+                following.find((f) => f.id === user.id) === undefined ? (
+                  <button className="likes-follow">Follow</button>
+                ) : (
+                  <button className="likes-unfollow">Following</button>
+                )
+              ) : null}
             </div>
-          </div>;
-        })}
+          ))}
       </div>
     </div>
   );
