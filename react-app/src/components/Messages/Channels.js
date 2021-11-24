052c41cba168f940a1f1c46883166880629148af
riousenkai <johnelijah.fajardo@gmail.com>
2021-11-23 18:47:58 -0600

    adding channels

---
+++ react-app/src/components/Messages/Channels.js
@@ -0,0 +1,9 @@
+const Channels = () => {
+    return <div className="channel-main">
+        <div className="channel-left">
+            
+        </div>
+    </div>
+}
+
+export default Channels
---
+++ react-app/src/components/Messages/Messages.js
@@ -0,0 +1,13 @@
+const Messages = ({ channel }) => {
+
+
+    if (!channel) {
+        return (
+            <div></div>
+        )
+    } else {
+        return (
+            <div></div>
+        )
+    }
+}
