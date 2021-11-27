788e1762013dae7967636dafa15e3fa3fd272560
riousenkai <johnelijah.fajardo@gmail.com>
2021-11-26 20:21:10 -0600

    notifications done

--- react-app/src/components/Notifications/NotificationsModal.js
+++ react-app/src/components/Notifications/NotificationsModal.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { NotifModal } from "../../context/NotifModal";
 import { useModal } from "../../context/UseModal";
 import Notifications from "./Notifications";
-import { notifIcon, notifIconFocus } from "../Navigation/NavIcons";
+import { notifIcon, notifIconFocus, newNotifs } from "../Navigation/NavIcons";
+import { readAllNotif, getUserNotif } from "../../store/notification";
 import "./Notifications.css";
 
 const NotificationsModal = () => {
   const { num, setNum } = useModal();
+  const dispatch = useDispatch();
+  const notifications = useSelector(
+    (state) => state.notification.notifications
+  );
+  const user = useSelector((state) => state.session.user);
+  const [newN, setNewN] = useState(false);
+
+  useEffect(() => {
+    dispatch(getUserNotif());
+  }, []);
+
+  useEffect(() => {
+    console.log(notifications);
+    const filtered = notifications?.filter(
+      (n) => n.read === false && n.sender.id !== user?.id
+    );
+    console.log(filtered);
+    if (filtered?.length > 0) {
+      setNewN(true);
+    }
+  }, notifications);
 
   return (
     <>
@@ -14,9 +37,27 @@ const NotificationsModal = () => {
         <span className="notif-icon" onClick={() => setNum(0)}>
           {notifIconFocus}
         </span>
-      ) : (
-        <span className="notif-icon" onClick={() => setNum(55)}>
+      ) : newN === false ? (
+        <span
+          className="notif-icon"
+          onClick={() => {
+            dispatch(readAllNotif());
+            setNum(55);
+            setNewN(false);
+          }}
+        >
           {notifIcon}
+        </span>
+      ) : (
+        <span
+          className="notif-icon"
+          onClick={() => {
+            dispatch(readAllNotif());
+            setNum(55);
+            setNewN(false);
+          }}
+        >
+          {newNotifs}
         </span>
       )}
 
