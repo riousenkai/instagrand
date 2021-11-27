a5fcdc466a3ebcd556e243aebad9e0f55694b339
riousenkai <johnelijah.fajardo@gmail.com>
2021-11-26 18:39:07 -0600

    fixed routes

--- app/api/like_routes.py
+++ app/api/like_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
-from app.models import Comment, User, Post, db, Like
+from app.models import Comment, User, Post, db, Like, Notification
 from .post_routes import following_posts
 
 like_routes = Blueprint('likess', __name__)
@@ -12,7 +12,10 @@ def like_post(id):
     like = Like.query.filter_by(user_id=current_user.id, post_id=id).first()
 
     if like is None:
+        post = Post.query.get(id)
+        new_notification = Notification(sender=current_user.id, message='liked your post.', user_id=post.user_id, link=f'/posts/{post.id}')
         new_like = Like(user_id=current_user.id, post_id=id)
+        db.session.add(new_notification)
         db.session.add(new_like)
         db.session.commit()
 
