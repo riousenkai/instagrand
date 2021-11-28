cf6b8817b0a272dc9cbd618e9591c04819b0c9fc
riousenkai <johnelijah.fajardo@gmail.com>
2021-11-28 00:04:41 -0600

    hopefully fixes database

---
+++ migrations/versions/20211128_000128_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: e9d5562481f8
+Revises: c8594756235c
+Create Date: 2021-11-28 00:01:28.767587
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e9d5562481f8'
+down_revision = 'c8594756235c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
