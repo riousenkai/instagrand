e886d142d7571bdd36db74d87612170688ac2d6c
riousenkai <johnelijah.fajardo@gmail.com>
2021-12-20 20:31:27 -0600

    fixed explore

--- react-app/src/components/Explore/Explore.js
+++ react-app/src/components/Explore/Explore.js
@@ -1,20 +1,63 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router";
 import { populateExplore } from "../../store/post";
 import "./Explore.css";
 
 const Explore = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
   const explorePosts = useSelector((state) => state.post.explore);
 
   useEffect(() => {
     dispatch(populateExplore());
-    console.log(explorePosts);
   }, []);
 
+  const loadIt = (i) => {
+    document.querySelector(`.pl-img-${i}`).classList.add("hidden");
+    document.querySelector(`.p-img-${i}`).classList.remove("hidden");
+  };
+
   return (
-    <div>
-      <div>None Here Yet</div>
+    <div className="explore-main">
+      <div className="explore-card">
+        {explorePosts?.length > 0 ? (
+          explorePosts?.map((post, i) => (
+            <div
+              className="post-c"
+              onClick={() => history.push(`/posts/${post.post.id}`)}
+            >
+              <img
+                className={`p-img-loading pl-img-${i} `}
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Loader.gif/480px-Loader.gif"
+              />
+              <img
+                className={`p-img p-img-${i} hidden`}
+                onLoad={() => loadIt(i)}
+                src={post.post.media_url}
+              />
+              <div className="p-hover">
+                <div className="p-likes">
+                  <img
+                    className="p-icons"
+                    src="https://img.icons8.com/fluency-systems-filled/48/ffffff/like.png"
+                  />
+                  <div className="p-like-ct">{post.likes}</div>
+                </div>
+                <div className="p-comments">
+                  <img
+                    className="p-icons"
+                    src="https://img.icons8.com/ios-filled/48/ffffff/speech-bubble.png"
+                  />
+                  <div className="p-like-ct">{post.comments}</div>
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div className="explore-none">No Available Posts</div>
+        )}
+      </div>
     </div>
   );
 };
